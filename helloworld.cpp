a797ad6885e690a9ad2f08062684cf32d528403a
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-09-05 16:33:10 -0400

    Add comments

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //header file library 
+using namespace std;  //we can use names for objects and variables from the standard library
 
-int main() {
-    cout << "Hello, world!";
-    return 0;
+
+int main() { //function
+    cout << "Hello, world!"; // << -> insertion operator
+    return 0;  //end main function
 }
