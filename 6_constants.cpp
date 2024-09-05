ebf4bd5139bd8b533d5de3ff92c12858ed33ff34
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-09-05 17:02:22 -0400

    constants

---
+++ 6_constants.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void constant(const int myNum) {
+    //myNum = 10; //error: constant means unchangeable and read-only
+}
+
+int main() {
+    return 0;
+}
