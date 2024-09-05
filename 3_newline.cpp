62d000a6767041bcd7d40d32951b091644d8c371
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-09-05 16:44:37 -0400

    newline

--- 3_newline.cpp
+++ 3_newline.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void n() {
+    cout << "\n";
+}
+
+void newline() {
     cout << "Hello,world!" << "\n";
     cout << "Have a nice day";
+}
+
+void newline_2() {
+    cout << "Hello,world!" << "\n\n";
+    cout << "Have a nice day";
+}
+
+void newline_3() {
+    cout << "Hello,world!" << endl;
+    cout << "Have a nice day";
+}
+
+int main() {
+    newline();
+    n();
+    newline_2();
+    n();
+    newline_3();
     return 0;
 }
