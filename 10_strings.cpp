dba64bd139cecd7c04cdad5825cd260520de621e
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-09-06 19:52:41 -0400

    commit strings

---
+++ 10_strings.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void string_1() {
+    string firstName = "John";
+    string lastName = "Doe";
+    string fullName = firstName + " " + lastName;
+    cout << fullName << endl;
+}
+
+void string_append() {
+    string firstName = "John";
+    string lastName = "Doe";
+    string fullName = firstName.append(lastName);
+    cout << fullName << endl;
+}
+
+void string_add() {
+    string x = "10";
+    string y = "20";
+    string z = x + y;
+    cout << z << endl;
+}
+
+void string_length() {
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the txt string is: " << txt.length() << endl;    
+}
+
+void string_access() {
+    string myString = "Hello";
+    cout << myString[0] << endl;
+
+    cout << myString[myString.length() - 1] << endl;
+
+    myString[0] = 'J';
+    cout << myString << endl;
+}
+
+void string_at() {
+    //at() Function 
+    string myString = "Hello";
+    cout << myString << endl;
+    cout << myString.at(0) << endl;
+    cout << myString.at(1) << endl;
+    cout << myString.at(myString.length() - 1) << endl;
+
+    myString.at(0) = 'J';
+    cout << myString << endl;
+}
+
+
+int main() {
+    // string_1();
+    // string_append();
+    // string_add();
+    // string_length();
+    // string_access();
+    string_at();
+    return 0;
+}
