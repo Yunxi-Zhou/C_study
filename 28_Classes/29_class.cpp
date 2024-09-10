3dc8da7047e31199c625d9b4c21e87b615a81421
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-09-10 17:47:34 -0400

    commit multiple objects

--- 28_Classes/29_class.cpp
+++ 28_Classes/29_class.cpp
@@ -7,6 +7,14 @@ class MyClass { //class
         string myString;  //Attribute
 };
 
+// Multiple Objects
+class Car {
+    public:
+        string brand;
+        string model;
+        int year;
+};
+
 int main() {
     MyClass myObj;
 
@@ -16,6 +24,21 @@ int main() {
     //print attribute values
     cout << myObj.myNum << endl;
     cout << myObj.myString << endl;
+
+    //create multiple objects
+    Car carObj1;
+    carObj1.brand = "BMW";
+    carObj1.model = "X5";
+    carObj1.year = 2000;
+
+    Car carObj2;
+    carObj2.brand = "Ford";
+    carObj2.model = "Fusion";
+    carObj2.year = 2301;
+
+    //Print multiple attributes values
+    cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << endl;
+    cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << endl;
 
     return 0;
 }
